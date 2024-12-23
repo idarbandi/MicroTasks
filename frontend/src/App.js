591109884c3e72f4a9c65477c6077ffbf732a8e3
idarbandi <darbandidr99@gmail.com>
2024-12-23 09:07:54 +0330

    set react app states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
-import { React, useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap';
+import axios from 'axios';
 
 function App() {
+  const [TodoList, setTodoList] = useState([{}]);
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+
   return (
-    <div className="App">
-      DarbanDev Proudly Presents
-      <div className="App"></div>
-      <div
-        className="App list-group-item justify-content-center align-items-center mx-auto"
-        style={{ width: '400px', backgroundcolour: 'white', MarginTop: '15px' }}
-      >
-        <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">
-          Task Manager
-        </h1>
-        <h6 className="card text-white bg-primary mb-3">FastApi - React - MongoDB</h6>
-        <div className="card-body">
-          <h5 className="card text-white bg-dark mb-3">Add Your Task</h5>
-        </div>
+    <div
+      className="App list-group-item
+          justyfy-content-center aligin-items-center
+          mx-auto"
+      style={{ width: '400px', backgroundColor: 'white', marginTop: '15px' }}
+    >
+      <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">
+        Task Manager
+      </h1>
+      <h6 className="card text-white bg-primary mb-3">FASTAPI - React - MangoDB</h6>
+      <div className="card-body">
+        <h5 className="card text-white bg-dark mb-3">Add Your Task</h5>
+        <span className="card-text">
+          <input className="mb-2 form-control titleIn" placeholder="Title" />
+          <input className="mb-2 form-control desIn" placeholder="Description" />
+          <button className="btn btn-outline-primary mx-2 mb-2" style={{ borderRadius: '50px', 'font-weight': 'bold' }}>
+            Add Task
+          </button>
+        </span>
+
+        <h5 className="card text-white bg-dark mb-3">Your Tasks</h5>
+        <div>{}</div>
       </div>
+
+      <h6 className="card text-white bg-warning py-1 mb-0">Copyright 2021, All rights reserved &copy;</h6>
     </div>
   );
 }
